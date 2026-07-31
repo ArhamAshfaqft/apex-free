2a79ffce809123416321c7b3f9c448a82fbcbe7b
ArhamAshfaqft <arham.ashfaqft@gmail.com>
2026-08-01 01:00:13 +0500

    Redesign toast notification with clean white container, soft ambient shadow, status icons, and remove left border

--- assets/js/editor-style-presets.js
+++ assets/js/editor-style-presets.js
@@ -202,15 +202,20 @@
     showToast: function (msg, type) {
       type = type || "success";
       var $toast = $("#apex-preset-toast");
+
+      var iconSvg = type === "error"
+        ? '<span class="apex-preset-toast-icon"><svg viewBox="0 0 24 24"><path fill="#ef4444" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/></svg></span>'
+        : '<span class="apex-preset-toast-icon"><svg viewBox="0 0 24 24"><path fill="#10b981" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg></span>';
+
       $toast
         .removeClass("apex-toast-success apex-toast-error")
         .addClass("apex-toast-" + type)
-        .text(msg)
+        .html(iconSvg + "<span>" + this.escapeHtml(msg) + "</span>")
         .addClass("apex-toast-active");
 
       setTimeout(function () {
         $toast.removeClass("apex-toast-active");
-      }, 3000);
+      }, 3200);
     },
 
     /**
